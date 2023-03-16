a3777b32df2814a218392dba7850f916cc4ae701
jayvannelli <jaymvannelli@gmail.com>
2023-03-16 08:55:15 -0400

    Add progress.

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,12 +1,38 @@
 import streamlit as st
 from src.data import get_data
 
+POSSIBLE_DISPLAY_VALUES = [
+    "Life Ladder", "Log GDP per capita", "Social support", "Healthy life expectancy at birth",
+    "Freedom to make life choices", "Generosity", "Perceptions of corruption",
+    "Positive affect", "Negative affect", "Confidence in national government"
+]
+
+
 
 def main():
     st.title("World Happiness Report 2005-2021 | Kaggle")
 
     df = get_data()
-    st.dataframe(df)
+
+    with st.expander("Data source"):
+        st.write("https://www.kaggle.com/datasets/jahaidulislam/world-happiness-report-2005-2021")
+
+    with st.expander("Display full pandas DataFrame"):
+        st.dataframe(df)
+
+    st.write("---")
+
+    left_column, right_column = st.columns(2)
+    with left_column:
+        country = st.selectbox("Select country", options=df['Country name'].unique())
+
+    with right_column:
+        display_value = st.selectbox("Select value to display",
+                                     options=POSSIBLE_DISPLAY_VALUES)
+
+    country_df = df.loc[df['Country name'] == country].set_index('Year')
+    st.subheader(f"{country} {display_value} ({country_df.index[0]} - {country_df.index[-1]})")
+    st.bar_chart(country_df, y=display_value)
 
 
 if __name__ == "__main__":
